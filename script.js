24ac93a2f053461879cbfaead8d5ab6a54bfe072
sistemsver <g6581468@gmail.com>
2025-05-17 11:32:26 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("userLoginButton").addEventListener("click", function() {
+    window.location.href = "https://gridy.jp/login";  // 通常ログイン画面へのリンク
+});
+
+document.getElementById("adminLoginButton").addEventListener("click", function() {
+    window.location.href = "https://gridy.jp/login";  // 管理者ログイン画面へのリンク
+});
